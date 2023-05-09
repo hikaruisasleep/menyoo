ce968bc4f74911962e33575bfb3f574c0c41469b
Ben Wu <ben@jxcb.xyz>
2023-05-09 13:31:02 +0700

    add title to head

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,14 +1,18 @@
 <script>
-	import '../app.postcss';
-	import '../app.scss';
+    import '../app.postcss';
+    import '../app.scss';
 </script>
 
-<nav class="container mx-auto py-4 bg-secondary-light">
-	<a href="/" class="bg-transparent p-0 text-black"
-		><h1 class="text-4xl text-center font-bold">menyoo_</h1></a
-	>
-</nav>
+<svelte:head>
+    <title>menyoo: Development Edition</title>
+</svelte:head>
+
+<div class="container mx-auto py-4 bg-secondary-light">
+    <a href="/" class="bg-transparent p-0 text-black"
+        ><h1 class="text-4xl text-center font-bold">menyoo_</h1></a
+    >
+</div>
 
 <div class="px-5">
-	<slot />
+    <slot />
 </div>
